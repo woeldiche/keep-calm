bac53de83279dade08598e73c9efaee2f02077b1
Jesper Wøldiche <jesper@woeldiche.dk>
2011-09-17 19:15:11 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>   
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<!--[if IE]><![endif]-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<!-- !CSS -->
+	<link rel="stylesheet" href="css/style.css?v=1">
+	<!-- Set cookie for adaptive images -->
+  <script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; expires=; path=/';</script>
+	<!-- !Modernizr -->
+	<script src="js/modernizr-1.5.min.js"></script>
+	<!-- Typekit -->
+	<script type="text/javascript" src="//use.typekit.com/omu1qun.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<!-- !Body -->
+<body>
+		<header>
+		  <img class="logo" src="/images/hm-druplicon-270.png" alt="This is joke combining Drupal and part of the Keep Calm And Carry On-posters" />
+		</header>
+		<section role="main" class="poster">
+		  <em>Keep Calm</em>
+		  <span>and</span>
+		  <em>Clear Cache</em>
+		</section><!-- /main -->
+		
+		<footer>
+      <!-- colophon information goes here eventually -->
+		</footer><!-- /footer -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	<script>!window.jQuery && document.write('<script src="js/jquery.min.js"><\/script>')</script>
+</body>
+</html>
